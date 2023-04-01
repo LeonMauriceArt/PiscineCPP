04bcf751c6c4584f963dae3d31c7b1c3d256f315
lmaurin- <leonard.maurin@gmail.com>
2023-04-01 16:58:35 +0200

    ex04 done need protections

---
+++ Module01/ex04/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int	main(int ac, char *av[])
+{
+	//check functions args and specify usage
+	if (ac != 4)
+	{
+		std::cout << "Not enough arguments, usage :" << std::endl;
+		std::cout << "./my_replace <filename> <string1> <string2>" << std::endl;
+		return (1);
+	}
+	//getting a string copy of file
+	std::ifstream file;
+	std::string	file_copy = "";
+	file.open(av[1], std::ios::in);
+	while (true)
+	{
+		std::string buff;
+		std::getline(file, buff);
+		if (file.fail())
+			break;
+		buff += "\n";
+		file_copy += buff;
+	}
+	std::string substr = av[2];
+	std::string replace_str = av[3];
+	size_t pos = file_copy.find(substr);
+	while (pos != std::string::npos)
+	{
+		file_copy = file_copy.substr(0, pos) + replace_str + file_copy.substr(pos + substr.length());
+		pos = file_copy.find(substr, pos + replace_str.length());
+	}
+	std::string filename = av[1];
+	filename += ".replace";
+	std::ofstream outfile(filename.c_str());
+	outfile << file_copy;
+	return (0);
+}
