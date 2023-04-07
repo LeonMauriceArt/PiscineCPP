09b7d9aa1c0cce40c08eb9b0b65542e3b57e75d8
lmaurin- <leonard.maurin@gmail.com>
2023-04-07 15:56:32 +0200

    module02 ex00 i dont understant

--- Module02/ex00/main.cpp
+++ Module02/ex00/main.cpp
@@ -1,22 +1,15 @@
 #include "Fixed.hpp"
 
-
-int main()
+int main(void)
 {
-	Fixed t1, t2;
-	t2 = t1;
-	Fixed t3 = t1;
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
 	return 0;
 }
-
-// int main(void)
-// {
-// 	Fixed a;
-// 	Fixed b(a);
-// 	Fixed c;
-// 	c = b;
-// 	std::cout << a.getRawBits() << std::endl;
-// 	std::cout << b.getRawBits() << std::endl;
-// 	std::cout << c.getRawBits() << std::endl;
-// 	return 0;
-// }
--- Module02/ex01/Fixed.hpp
+++ Module02/ex01/Fixed.hpp
@@ -7,14 +7,18 @@ class Fixed
 {
     private:
         int fixedpointnum;
-		static const int fractional_bits = 8;
+		static const int bits = 8;
 	public:
+		Fixed(const int num);
+		Fixed(const float num);
 		Fixed();
 		~Fixed();
 		Fixed(const Fixed& f);
 		Fixed& operator=(const Fixed& f);
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
 };
 
 #endif
