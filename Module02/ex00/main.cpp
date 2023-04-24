522a423ba2b8d4173b91b1002baec9c4e9afdb0e
lmaurin- <leonard.maurin@gmail.com>
2023-04-24 18:58:14 +0200

    need checking on module02 and revisions

--- Module02/ex00/main.cpp
+++ Module02/ex00/main.cpp
@@ -8,6 +8,7 @@ int main(void)
 
 	c = b;
 
+	a.setRawBits(5);
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
--- Module02/ex01/main.cpp
+++ Module02/ex01/main.cpp
@@ -4,11 +4,13 @@ int main(void)
 {
 	Fixed a;
 	Fixed const b(10);
-	Fixed const c(42.42f);
+	Fixed c(7.5f);
 	Fixed const d(b);
 
 	a = Fixed(1234.4321f);
 
+	c.setRawBits(5126);
+	std::cout << "c tests : rawbits = " << c.getRawBits() << std::endl;
 	std::cout << "a is " << a << " as float" << std::endl;
 	std::cout << "b is " << b << " as float" << std::endl;
 	std::cout << "c is " << c << " as float" << std::endl;
@@ -19,5 +21,6 @@ int main(void)
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	
+	std::cout << (float)(10860 >> 8) << std::endl;
 	return 0;
 }
