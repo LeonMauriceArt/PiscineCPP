751fb5318fd53b6c2de5a57919107e6f81d10d93
lmaurin- <leonard.maurin@gmail.com>
2023-07-17 19:57:59 +0200

    module06 almost done

--- Module06/ex02/main.cpp
+++ Module06/ex02/main.cpp
@@ -3,8 +3,13 @@
 #include "./includes/B.hpp"
 #include "./includes/C.hpp"
 
-#include <ctime>
+#include <time.h>
 #include <iostream>
+#include <stdlib.h>
+
+Base::~Base()
+{
+}
 
 Base *generate(void)
 {
@@ -20,15 +25,27 @@ Base *generate(void)
 
 void identify(Base *p)
 {
-	
+	A *testA = dynamic_cast<A*>(p);
+	B *testB = dynamic_cast<B*>(p);
+	C *testC = dynamic_cast<C*>(p);
+	if (testA)
+		std::cout << "Object is of A type" << std::endl;
+	else if (testB)
+		std::cout << "Object is of B type" << std::endl;
+	else if (testC)
+		std::cout << "Object is of C type" << std::endl;
+	else
+		std::cout << "Error : object has no type" << std::endl;
 }
 
-void identify(Base &p)
-{
+// void identify(Base &p)
+// {
 
-}
+// }
 
 int main(void)
 {
+	Base *test = generate();
+	identify(test);
 	return (0);
 }
