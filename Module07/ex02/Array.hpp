26c40487c6404aea3090524ca847b95849234b20
lmaurin- <leonard.maurin@gmail.com>
2023-07-31 20:39:56 +0200

    07 almost ok

--- Module07/ex02/Array.hpp
+++ Module07/ex02/Array.hpp
@@ -6,24 +6,102 @@
 #include <exception>
 #include <iostream>
 
-template <class T>
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+template <typename T>
 class Array
 {
 	private:
-		T tab[];
+		T *_tab;
+		unsigned int _size;
 	public:
 		class OutOfArray: public std::exception
 		{
 			public :
-				virtual const char* what() const throw(); 
+				virtual const char* what() const throw(){
+					return ("Error : Out of array !");
+				}
 		};
-		Array();
-		Array(unsigned int n);
-		Array(const Array &other);
-		Array &operator=(const Array &other);
-		~Array();
-		size_t size() const;
-		T operator[](size_t index);
+		Array(): _size(0){
+			this->_tab = new T();
+		}
+		Array(unsigned int n): _size(n)
+		{
+			this->_tab = new T[n];
+		}			
+		Array(const Array &other): _size(other.size())
+		{
+			if (other._tab)
+			{
+				this->_tab = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+					this->_tab[i] = other._tab[i];
+			}
+			else 
+				this->_tab = new T[0];
+		}
+		Array &operator=(const Array &other)
+		{
+			if (this->_tab)
+				delete(this->_tab);
+			if (other._tab)
+			{
+				this->_size = other.size();
+				this->_tab = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+					this->_tab[i] = other._tab[i];
+			}
+			else
+			{
+				this->_size = 0;
+				this->_tab = new T[0];
+			}
+			return(*this);
+		}
+		~Array()
+		{
+			if (this->_tab)
+			delete(this->_tab);
+		}
+		unsigned int size() const
+		{
+			return(this->_size);
+		}
+		T &operator[](unsigned int index)
+		{
+			try 
+			{
+				if (index < 0 || index > _size)
+					throw OutOfArray();
+				return (_tab[index]);
+			} 
+			catch (std::exception &e) 
+			{
+				std::cout << RED << e.what() << RESET << std::endl;
+				return _tab[0];
+			}
+		}
+		void displayArray()
+		{
+			for(unsigned int i = 0; i < _size; i++)
+				std::cout << _tab[i] << std::endl;
+		}
 };
 
 
--- Module07/ex02/main.cpp
+++ Module07/ex02/main.cpp
@@ -1,7 +1,40 @@
 #include "Array.hpp"
 
+void intTest()
+{
+	std::cout << YELLOW << "---Int Array Test---" << RESET << std::endl;
+	Array<int> intArray(5);
+	intArray[5] = 12;
+	std::cout << "Array size = " << intArray.size() << std::endl;
+	std::cout << intArray[5] << std::endl;
+	std::cout << intArray[-5] << std::endl;
+	Array<int> test(10);
+	test[5] = 42;
+	test.displayArray();
+	{
+		Array<int> copy;
+		copy = test;
+		std::cout << test[5] << " - " << copy[5] << std::endl;
+	}
+	std::cout << test[5] << std::endl;
+}
+
+void charTest()
+{
+	std::cout << YELLOW << "---Char Array Test---" << RESET << std::endl;
+	Array<char> charArray(1);
+	charArray[1] = 'F';
+	Array<char> test(charArray);
+	charArray.displayArray();
+	test.displayArray();
+	// Array<char> empty;
+	// std::cout << "empty " << empty[0] << std::endl;
+	// std::cout << empty[1] << std::endl;
+}
+
 int main(void)
 {
-	Array<int> intArray(9);
-	std::cout << intArray[2] << std::endl; 
+	intTest();
+	charTest();
+	return(0);
 }
