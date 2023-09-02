3f3874562f6028e87b90f34d988e7c9017823fdc
lmaurin- <leonard.maurin@gmail.com>
2023-09-02 20:28:01 +0200

    work on cpp9

--- Module09/ex01/RPN.hpp
+++ Module09/ex01/RPN.hpp
@@ -1,12 +1,20 @@
 #ifndef RPN_HPP
 #define RPN_HPP
 
+#include <iostream>
+#include <stack>
+
 class RPN
 {
 	private:
-
+		std::stack<int> numStack;
+		RPN();
+		RPN(const RPN &other);
+		RPN &operator=(const RPN &other);
 	public:
-	
+		RPN
+		~RPN();
+		void	reversePolishCalculation(char **av);
 };
 
 
--- Module09/ex01/main.cpp
+++ Module09/ex01/main.cpp
@@ -1,6 +1,23 @@
 #include "RPN.hpp"
 
+bool	checkInput(std::string expression)
+{
+	(void)expression;
+	return (true);
+}
+
 int main(int ac, char **av)
 {
+	if (ac != 2)
+	{
+		std::cout << "Error: need a reverse polish expression as argument." << std::endl << "Example: \"1 1 +\"" << std::endl;
+		return (1);
+	}
+	std::string expression(av[1]);
+	if (!checkInput(expression))
+	{
+		std::cout << "Error: bad input." << std::endl;
+		return (1);
+	}
 	return (0);
 }
